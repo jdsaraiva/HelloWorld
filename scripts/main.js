ad31578921368b82af1649df168a43ea08dca696
João Dessain Saraiva <joao@dessaindesign.com>
2013-01-17 16:19:32 +0000

    fwa

--- scripts/main.js
+++ scripts/main.js
@@ -8,25 +8,25 @@ function setOrientationListener(){
 
 function updateOrientation(){	
 	if($('body').width() < 1024){  /*Landscape  funçao remove e add só apanhar a partir da classe pai*/
-		$('.seccaoInicio,.seccaoShout').removeClass('landscape').addClass('portrait');
+		$('.seccaoInicio,.seccaoShout,.itemMenu').removeClass('landscape').addClass('portrait');
 	}else{
-		$('.seccaoInicio,.seccaoShout').removeClass('portrait').addClass('landscape');			
+		$('.seccaoInicio,.seccaoShout,.itemMenu').removeClass('portrait').addClass('landscape');			
 	}
 }
 
 function carregar(event) {
  if (event.target.id == 1) {   
   $('.contentor').animate({'margin-left': '0%'}, 900);  
-  $("#barraMenu").animate({"left":"19"}, 900);
+  $(".barraMenu").animate({"left":"19"}, 900);
  }
   if (event.target.id == 2) {  
   $('.contentor').animate({'margin-left': '-100%'}, 900);
-  $("#barraMenu").animate({"left":"110"}, 900);
+  $(".barraMenu").animate({"left":"110"}, 900);
  }
    
    if (event.target.id == 3) {  
   $('.contentor').animate({'margin-left': '-200%'}, 900);
-    $("#barraMenu").animate({"left":"210"}, 900);
+    $(".barraMenu").animate({"left":"210"}, 900);
  }    
   };
 
